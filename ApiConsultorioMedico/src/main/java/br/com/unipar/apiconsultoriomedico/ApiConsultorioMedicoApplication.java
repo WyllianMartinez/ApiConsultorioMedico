a088912b63eb9ea668ab40f2706e0099e150dfee
WyllianMartinez <110701155+WyllianMartinez@users.noreply.github.com>
2023-06-04 10:32:20 -0300

    new packages

---
+++ ApiConsultorioMedico/src/main/java/br/com/unipar/apiconsultoriomedico/ApiConsultorioMedicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.unipar.apiconsultoriomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultorioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultorioMedicoApplication.class, args);
+	}
+
+}
---
+++ ApiConsultorioMedico/src/test/java/br/com/unipar/apiconsultoriomedico/ApiConsultorioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unipar.apiconsultoriomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultorioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
